f651a00c60a7438a28c43b0a45da347dca7afb78
Braulio Disla <brauliodisla03@gmail.com>
2021-07-14 21:02:45 -0400

    Agregar archivos de proyecto.

---
+++ programas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace programas
+{
+    class Program
+    {
+
+        public static void limpiarpantalla()
+        {
+            Console.WriteLine("Bienvenido al programa");
+            Console.WriteLine(" de ejemplo");
+            Console.WriteLine("Espero que estés bien");
+        }
+
+        static void Main(string[] args)
+        {
+
+            limpiarpantalla();
+
+        }
+
+       
+
+    }
+}
